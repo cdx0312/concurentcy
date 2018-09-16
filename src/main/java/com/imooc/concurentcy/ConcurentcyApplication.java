e5c79937ea1d0952e3e242003167af527e598d7c
cdx <cdxu0312@outlook.com>
2018-09-16 18:59:08 +0800

    Initial commit

---
+++ src/main/java/com/imooc/concurentcy/ConcurentcyApplication.java
@@ -0,0 +1,12 @@
+package com.imooc.concurentcy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurentcyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurentcyApplication.class, args);
+    }
+}
---
+++ src/main/java/com/imooc/concurentcy/annotations/Recommend.java
@@ -0,0 +1,18 @@
+package com.imooc.concurentcy.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @program: concurentcy
+ * @author: cdx
+ * @create: 2018-09-16 18:54
+ * 用来标记线程推荐的的类的注解
+ **/
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Recommend {
+    String value() default "";
+}
